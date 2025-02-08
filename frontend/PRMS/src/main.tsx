7050a56fc546b3a53c96d373cf64c00144be04a5
RachedSouihi <Souihiraached@gmail.com>
2025-02-08 17:48:24 +0100

    Set up initial frontend architecture

---
+++ frontend/PRMS/src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+import "./assets/styles/global.scss"
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
---
+++ frontend/PRMS/src/pages/SignUp/SignUp.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+
+const  SignUp: FC = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SignUp;
