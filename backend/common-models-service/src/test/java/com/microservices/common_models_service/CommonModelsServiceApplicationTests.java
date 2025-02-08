d12b977f271255fbab918360964a29b168db7d2d
RachedSouihi <Souihiraached@gmail.com>
2025-02-08 17:51:49 +0100

    Set up initial backend architecture with microservices

---
+++ backend/common-models-service/src/test/java/com/microservices/common_models_service/CommonModelsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.common_models_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonModelsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ backend/printrequest-service/src/main/java/com/microservices/printrequest_service/PrintrequestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.printrequest_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintrequestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintrequestServiceApplication.class, args);
+	}
+
+}
