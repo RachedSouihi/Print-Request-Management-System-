50608592479b7cd0de5518365d53292af5ae2122
RachedSouihi <Souihiraached@gmail.com>
2025-02-08 19:06:59 +0100

    Create authentication.ts
    Create authentication file with signup and login data types

---
+++ frontend/PRMS/src/types/authentication.ts
@@ -0,0 +1,14 @@
+export interface SignUpFormData {
+    firstName: string;
+    lastName: string;
+    phone: string;
+    educationLevel: string;
+    email: string;
+    password: string;
+}
+
+
+export interface LoginFormData{
+    email: String;
+    password: String
+}
