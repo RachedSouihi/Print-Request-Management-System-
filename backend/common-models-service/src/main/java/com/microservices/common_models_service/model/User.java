3c54ff4bba8442f844ec9460cb30bcab5bf3b723
RachedSouihi <Souihiraached@gmail.com>
2025-02-10 13:53:56 +0100

    Create Profile and User entities

---
+++ backend/common-models-service/src/main/java/com/microservices/common_models_service/model/User.java
@@ -0,0 +1,21 @@
+package com.microservices.common_models_service.model;
+
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.UuidGenerator;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users") // Rename the table to "users"
+public class User {
+    @Id
+    @GeneratedValue
+    @UuidGenerator(style = UuidGenerator.Style.RANDOM)
+    private UUID user_id;
+
+    private String email;
+    private String password;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private Profile profile;
+}
