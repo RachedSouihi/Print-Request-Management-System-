22a8b20365a874f7ca2e48a60edfce7a8bcc198c
RachedSouihi <Souihiraached@gmail.com>
2025-02-11 15:58:08 +0100

    Update entities

--- backend/common-models-service/src/main/java/com/microservices/common_models_service/model/User.java
+++ backend/common-models-service/src/main/java/com/microservices/common_models_service/model/User.java
@@ -1,21 +1,45 @@
 package com.microservices.common_models_service.model;
 
-
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
-import org.hibernate.annotations.UuidGenerator;
-import java.util.UUID;
 
 @Entity
-@Table(name = "users") // Rename the table to "users"
+@Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue
-    @UuidGenerator(style = UuidGenerator.Style.RANDOM)
-    private UUID user_id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int user_id; // No @GeneratedValue because the ID is manually assigned
 
     private String email;
     private String password;
 
-    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+   @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
     private Profile profile;
+
+    // Getters and Setters
+    public int getUser_id() {
+        return user_id;
+    }
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public Profile getProfile() {
+        return profile;
+    }
+    public void setProfile(Profile profile) {
+        this.profile = profile;
+    }
 }
