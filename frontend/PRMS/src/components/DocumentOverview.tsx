a002c80b29ee939679c3077f107572cdd87c71ff
RachedSouihi <Souihiraached@gmail.com>
2025-02-11 18:43:55 +0100

    Added new components

---
+++ frontend/PRMS/src/components/DocumentOverview.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DocumentOverview() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ frontend/PRMS/src/features/ProductCart.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ProductCart(document: any) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
