69777c3fa6ce84ce5ce9f6defd2efa39c5e79c3b
RachedSouihi <Souihiraached@gmail.com>
2025-02-11 16:00:38 +0100

    Map datasourse to user-service

---
+++ backend/user-service/src/main/java/com/microservices/user_service/config/JpaConfig.java
@@ -0,0 +1,32 @@
+package com.microservices.user_service.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class JpaConfig {
+
+
+    @Bean
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .url("jdbc:postgresql://localhost:5432/prms")
+                .username("postgres")
+                .password("")
+                .driverClassName("org.postgresql.Driver")
+                .build();
+    }
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan("com.microservices.common_models_service.model");
+        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        return em;
+    }
+}
