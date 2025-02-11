3542041ae2b02dc0b17b1dafc26f262fb0043293
RachedSouihi <Souihiraached@gmail.com>
2025-02-11 15:59:47 +0100

    add signup method

--- backend/user-service/src/main/java/com/microservices/user_service/controller/UserController.java
+++ backend/user-service/src/main/java/com/microservices/user_service/controller/UserController.java
@@ -1,19 +1,56 @@
 package com.microservices.user_service.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.microservices.common_models_service.model.Profile;
+import com.microservices.common_models_service.model.User;
+import com.microservices.user_service.service.UserService;
+import jakarta.ws.rs.QueryParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
+    private final UserService userService;
 
 
-
-    @GetMapping
-    public String getUser() {
-        return "Hello World";
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
+
+
+
+
+
+
+    @GetMapping("/signup")
+        public ResponseEntity<?> signup(@RequestBody User user) {
+        try{
+            return ResponseEntity.ok(userService.signUp(user));
+
+
+
+        }catch(Exception e){
+
+            return ResponseEntity.ok(e.getMessage());
+
+        }
+
+        }
+
+
+
+
+
+        @GetMapping("/get-token")
+    public ResponseEntity<Map<String, Object>> getToken(@QueryParam("username") String username, @QueryParam("password") String password) throws Exception {
+        return ResponseEntity.ok(userService.getToken(username, password));
+    }
+
 }
