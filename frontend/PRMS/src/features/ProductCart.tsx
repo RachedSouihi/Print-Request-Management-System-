74559f25661dcf5757d515154b444c6d67d2f769
jahmi-chaima <jahmichaima333@gmail.com>
2025-02-13 12:50:59 +0100

    Sauvegarde des modifications

---
+++ frontend/PRMS/src/features/ProductCart.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+
+interface DocumentType {
+  title: string;
+  description: string;
+  image: string;
+}
+
+interface ProductCardProps {
+  document: DocumentType;
+}
+
+const ProductCard: React.FC<ProductCardProps> = ({ document }) => {
+  return (
+    <Card className="custom-card shadow-lg border-0">
+      <div className="image-container">
+        <Card.Img variant="top" src={document.image} alt={document.title} className="card-img" />
+      </div>
+      <Card.Body className="d-flex flex-column">
+        <Card.Title className="fw-bold">{document.title}</Card.Title>
+        <Card.Text className="text-muted">{document.description}</Card.Text>
+        <div className="mt-auto d-flex justify-content-between">
+          <Button variant="outline-primary">Preview</Button>
+          <Button variant="primary">Download</Button>
+        </div>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default ProductCard;
