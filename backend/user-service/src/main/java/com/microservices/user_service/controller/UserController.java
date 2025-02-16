7e59b3e0018ea47da6bd870dddfce1cb39a354c4
jahmi-chaima <jahmichaima333@gmail.com>
2025-02-16 12:49:41 +0100

    Fusion des fonctionnalités d'authentification et d'inscription

--- backend/user-service/src/main/java/com/microservices/user_service/controller/UserController.java
+++ backend/user-service/src/main/java/com/microservices/user_service/controller/UserController.java
@@ -147,6 +147,155 @@ public class UserController {
         }
     }
 
+package com.microservices.user_service.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microservices.common_models_service.model.Profile;
+import com.microservices.common_models_service.model.User;
+import com.microservices.user_service.service.*;
+import com.microservices.user_service.utils.AESUtil;
+import com.microservices.user_service.utils.VerificationCodeUtil;
+import com.microservices.user_service.utils.VerificationData;
+import jakarta.mail.MessagingException;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.Duration;
+import java.util.*;
+
+@RestController
+@EnableJpaRepositories(basePackages = "com.microservices.common-models.repository")
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+@RequestMapping("/user")
+public class UserController {
+
+    private final AuthService authService;
+    private final UserService userService;
+    private final EmailService emailService;
+    private final VerificationService verificationService;
+    private final ObjectMapper objectMapper;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+    @Value("${app.secret.key}")
+    private String SECRET_KEY;
+
+    @Value("${app.encrytion.algo}")
+    private String ALGO;
+
+    @Autowired
+    public UserController(AuthService authService, UserService userService, EmailService emailService,
+                          VerificationService verificationService, ObjectMapper objectMapper) {
+        this.authService = authService;
+        this.userService = userService;
+        this.emailService = emailService;
+        this.verificationService = verificationService;
+        this.objectMapper = objectMapper;
+    }
+
+    // Méthode de connexion
+    @GetMapping("/login")
+    public ResponseEntity<?> login(@RequestParam("username") String username, @RequestParam("password") String password) {
+        try {
+            String token = authService.login(username, password);
+            return ResponseEntity.ok().body("{\"access_token\": \"" + token + "\"}");
+        } catch (Exception e) {
+            return ResponseEntity.status(401).body("{\"error\": \"" + e.getMessage() + "\"}");
+        }
+    }
+
+    // Génération de clé secrète
+    @GetMapping("/generate-secret-key")
+    public ResponseEntity<?> generateSecretKey() throws Exception {
+        String secret_key = AESUtil.generateSecretKeyBase64();
+        return ResponseEntity.ok(secret_key);
+    }
+
+    // Vérification du mot de passe encrypté
+    @GetMapping("/check-pwd")
+    public ResponseEntity<String> checkPassword(@RequestBody Map<String, String> request) {
+        try {
+            String encryptedPayload = request.get("encryptedPayload");
+            String decryptedPayload = AESUtil.decrypt(encryptedPayload, SECRET_KEY, ALGO);
+            Map<String, Object> payloadMap = objectMapper.readValue(decryptedPayload, HashMap.class);
+
+            String password = (String) payloadMap.get("password");
+            long timestamp = (long) payloadMap.get("timestamp");
+
+            long currentTime = new Date().getTime();
+            long timeDifference = currentTime - timestamp;
+            long expirationMillis = 1 * 60 * 1000;
+
+            if (timeDifference > expirationMillis) {
+                return ResponseEntity.status(401).body("Payload expired");
+            } else {
+                return ResponseEntity.ok("Payload valid");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Decryption failed");
+        }
+    }
+
+    // Inscription d'un utilisateur
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody Map<String, Object> request, HttpServletResponse response) {
+        try {
+            String otp = objectMapper.convertValue(request.get("otp"), String.class);
+            User user = objectMapper.convertValue(request.get("user"), User.class);
+            VerificationData data = verificationService.getVerificationData(user.getEmail());
+            user.setPassword(data.getHashedPassword());
+
+            if (otp.equals(data.getCode())) {
+                Map<String, Object> tokens = userService.signUp(user);
+                ResponseCookie cookie1 = ResponseCookie.from("token", tokens.get("access_token").toString())
+                        .httpOnly(true).sameSite("Lax").path("/").maxAge(Duration.ofDays(2)).build();
+                ResponseCookie cookie2 = ResponseCookie.from("token", tokens.get("access_token").toString())
+                        .httpOnly(true).sameSite("Lax").path("/").maxAge(Duration.ofDays(10)).build();
+
+                response.addHeader(HttpHeaders.SET_COOKIE, cookie1.toString());
+                response.addHeader(HttpHeaders.SET_COOKIE, cookie2.toString());
+
+                return ResponseEntity.ok().body("Login successful");
+            }
+            return ResponseEntity.status(401).body("Invalid otp");
+        } catch (Exception e) {
+            return ResponseEntity.ok().body(e.getMessage());
+        }
+    }
+
+    // Envoi d'un e-mail de vérification
+    @PostMapping("/verify-email")
+    public ResponseEntity<?> sendEmail(@RequestBody Map<String, String> request) throws MessagingException {
+        String email = request.get("email");
+        String password = request.get("password");
+
+        VerificationData data = new VerificationData();
+        data.setCode(VerificationCodeUtil.generateVerificationCode(4));
+        data.setEmail(email);
+        data.setHashedPassword(bCryptPasswordEncoder.encode(password));
+
+        verificationService.storeVerificationData(email, data);
+        emailService.sendVerificationEmail(data.getEmail(), data.getCode(), request.get("firstname"));
+        return ResponseEntity.ok("Email sent");
+    }
+
+    // Vérification de l'état de connexion
+    @GetMapping("/signed-in")
+    public String signinPage(Authentication authentication) {
+        if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+            return "not signed in";
+        }
+        return "signed in";
+    }
+}
     @PostMapping("/signup")
     public ResponseEntity<?> signup(@RequestBody Map<String, Object> request, HttpServletResponse response) {
         try {
