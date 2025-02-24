a63a5306acfd0d6b5c56c916ce578768f8cdca52
RachedSouihi <Souihiraached@gmail.com>
2025-02-24 11:40:11 +0100

    Added documentSlice for getting docs

--- frontend/PRMS/src/pages/Home/Home.tsx
+++ frontend/PRMS/src/pages/Home/Home.tsx
@@ -1,26 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../../layouts/Header";
-import ExamsOverview from "../../components/ExamsOverview";
-import TestimonialCarousel from "../../components/Testimonials";
 import Footer from "../../layouts/Footer";
 import Sidebar from "../../components/SideBar/SideBar";
 import { Col } from "react-bootstrap";
-import CustomToast from "../../common/Toast";
+import { PrintRequestModal } from "../../components/PrintRequest/PrintRequest";
+import axios from "axios";
+import { Outlet } from "react-router";
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '../../store/store';
+import { fetchDocuments } from '../../store/documentsSlice';
 
 const Home: React.FC = () => {
-  const [showToast, setShowToast] = useState<boolean>(true);
-  const [toastType, setToastType] = useState<'success' | 'danger' | 'warning'>('success');
-  const [toastMessage, setToastMessage] = useState<string>('Operation completed successfully!');
 
+
+  const dispatch: AppDispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchDocuments());
+  }, [dispatch]);
+
+ 
   return (
     <div className="d-flex gap-0">
-      <CustomToast
-        show={showToast}
-        onClose={() => setShowToast(false)}
-        type={toastType}
-        message={toastMessage}
-      />
-
       <Col md={3}>
         <Sidebar />
       </Col>
@@ -28,10 +29,9 @@ const Home: React.FC = () => {
       <Col md={9}>
         <Header />
 
-        <ExamsOverview />
+        <Outlet />
 
-        <TestimonialCarousel />
-
+  
         <Footer />
       </Col>
     </div>
